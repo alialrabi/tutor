37804a4a8f26361eed03555db171f0f0f8ef1bb8
od1000174 <aalrabi@ntgclarity.com>
2026-03-14 04:37:10 +0200

    book timeslot

--- src/main/java/com/tutor/business/dto/SessionDto.java
+++ src/main/java/com/tutor/business/dto/SessionDto.java
@@ -3,6 +3,7 @@ package com.tutor.business.dto;
 import com.tutor.common.dto.BaseDto;
 import com.tutor.persistance.entity.Tutor;
 import com.tutor.persistance.entity.UserProfile;
+import jakarta.persistence.Column;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Getter
@@ -18,6 +20,8 @@ public class SessionDto {
     private Long tutorId;
     private TutorDto tutor;
     private UserDto userProfile;
-    private TimeSlotDto timeSlot;
     private String roomId;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
 }
